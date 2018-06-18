4f377208663915e54d39b4cc3caabd6f1619a638
paswd <paswd@yandex.ru>
2018-06-18 11:31:53 +0300

    Added final check

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 int main(void) {
-	const int hashbitlen = 256;
-	BitSequence reshash[hashbitlen / 8];
-	const DataLength dlen = 256;
+	const int hashbitlen = 512;
+	const DataLength dlen = hashbitlen / 8;
+	BitSequence reshash[dlen];
 	BitSequence data[dlen];
-	int rounds = 42;
+	//int rounds = 42; //original
 	cin >> data;
-	HashReturn status = Hash(hashbitlen, data, dlen, reshash, rounds);
-	cout << reshash << endl;
+	for (int rounds = 10; rounds <= 60; rounds++) {
+		size_t change_cnt = 0;
+		HashReturn status = Hash(hashbitlen, data, dlen, reshash, rounds);
+		for (size_t i = 0; i < (size_t) dlen; i++) {
+			if (data[i] != reshash[i]) {
+				change_cnt++;
+			}
+		}
+		cout << rounds << "\t" << change_cnt << endl;
+	}
 	return 0;	
 }
