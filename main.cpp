3d1e49a9be370ea75d5f79f91b66d493c29f1b9c
paswd <paswd@yandex.ru>
2018-06-18 18:31:08 +0300

    Fixed bugs

--- main.cpp
+++ main.cpp
@@ -3,28 +3,38 @@
 #include "jh_ref.h"
 
 using namespace std;
+const int HASHBITLEN = 512;
+const int BYTE_SIZE = 8;
+const int ROUNDS_RANGE_BEGIN = 10;
+const int ROUNDS_RANGE_END = 61;
 
 int main(void) {
-	const int hashbitlen = 512;
-	const int byte_size = 8;
-	const DataLength dlen = hashbitlen / byte_size;
+	const DataLength dlen = HASHBITLEN / BYTE_SIZE;
 	BitSequence reshash[dlen];
 	BitSequence data[dlen];
 	//int rounds = 42; //original
 	cin >> data;
-	for (int rounds = 10; rounds <= 60; rounds++) {
+	size_t summ = 0;
+	for (int rounds = ROUNDS_RANGE_BEGIN; rounds < ROUNDS_RANGE_END; rounds++) {
 		size_t change_cnt = 0;
-		HashReturn status = Hash(hashbitlen, data, dlen, reshash, rounds);
+		HashReturn status = Hash(HASHBITLEN, data, dlen, reshash, rounds);
 		for (size_t i = 0; i < (size_t) dlen; i++) {
-			if (data[i] != reshash[i]) {
-				change_cnt += byte_size;
+			BitSequence mask = 1;
+			for (size_t j = 0; j < BYTE_SIZE; j++) {
+				if ((data[i] & mask) != (reshash[i] & mask)) {
+					change_cnt++;
+				}
+				mask = mask << 1;
 			}
 		}
 		if (status == SUCCESS) {
+			summ += change_cnt;
 			cout << rounds << "\t" << change_cnt << endl;
 		} else {
 			cout << "ERROR" << endl;
 		}
 	}
-	return 0;	
+	long double middle = (long double) summ / (long double) (ROUNDS_RANGE_END - ROUNDS_RANGE_BEGIN);
+	cout << endl << "Middle = " << middle << endl;
+	return 0;
 }
